16a65db3a9653e0f879b42a62e03f34bdf169513
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2022-09-18 23:38:35 +0200

    Part one done!

--- part1.py
+++ part1.py
@@ -1,57 +1,91 @@
+import json
+from urllib.parse import parse_qsl, urlsplit
+
 import PySimpleGUI as sg
 from scholarly import ProxyGenerator, scholarly
+from serpapi import GoogleSearch
+
+API_KEY = "5f6b35250a77810b11e6d8a918abeb62e6f37fc18dd836f9ca17ebcb7d4269e6"
 
 pg = ProxyGenerator()
 success = pg.FreeProxies()
 scholarly.use_proxy(pg)
 
 
-def getAuthorArticles(authorName,sorting):
+def getAuthorArticles(authorName, sorting):
     if sorting == 'Year':
-        sorting = 'year'
+        sorting = 'pubdate'
     else:
-        sorting = 'citedby'
+        sorting = ''
 
     search_query = scholarly.search_author(authorName)
     author = next(search_query)
-    articles = scholarly.fill(
-        author, sections=['basics','publications','indices','coauthors','counts'], sortby=sorting)
 
-    # Correct len
-    data = []
-    citedby = articles['citedby']
+    citedby = author['citedby']
+    author_id = author['scholar_id']
+
+    params = {
+        "engine": "google_scholar_author",
+        "author_id": author_id,
+        "api_key": API_KEY,
+        "sort": sorting,
+        "hl": "en",
+        "start": 0,
+        "num": 100
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+        
+    print(json.dumps(results,indent=2,ensure_ascii=False))
     
-    for index,pub in enumerate(articles['publications']):
-        row = []
-        extended_pub = scholarly.fill(pub)
+    data = []
+    while True:
+        res = search.get_dict()
+        for article in res.get("articles",[]):
+            row = []
+            row.append(article.get("title",''))
+            row.append(article.get("authors",''))
+            row.append(article.get("year",-1))
+            row.append(article.get("cited_by").get("value"))
 
-        try:
-            row.append(extended_pub['bib']['title'])
-        except KeyError:
-            row.append('')
+            data.append(row)
 
-        try:
-            row.append(extended_pub['bib']['author'])
-        except KeyError:
-            row.append('')
+        if "next" in res.get("serpapi_pagination",[]):
+            search.params_dict.update(dict(parse_qsl(urlsplit(res.get("serpapi_pagination").get("next")).query)))
+        else:
+            break
 
-        try:
-            row.append(extended_pub['bib']['pub_year'])
-        except KeyError:
-            row.append(-1)
+    return (data,citedby)
 
-        try:
-            row.append(pub['num_citations'])
-        except KeyError:
-            row.append(-1)
+def windowModal():
+    layout = [
+        [sg.Button('Authors articles', key='-ART-',
+                   auto_size_button=False, size=(60, 2))],
+        [sg.Button('Authors histogram', key='-HIST-',
+                   auto_size_button=False, size=(60, 2))],
+        [sg.Button('Cited papers histogram', key='-CITE-',
+                   auto_size_button=False, size=(60, 2))]
+    ]
 
-        data.append(row)
+    window = sg.Window('Window Modal', layout, size=(
+        300, 200), element_justification='c')
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
+
+        if event == '-ART-':
+            windowArt()
+        elif event == '-HIST-':
+            pass
+        elif event == '-CITE-':
+            pass
+    window.close()
 
 
-    return (data, citedby)
-
-
-def createLayout():
+def windowArt():
     layout = [
         [sg.Text("GUI for Google Scholar queries")],
         [sg.Text('Enter Author name'), sg.InputText(do_not_clear=False)],
@@ -61,22 +95,14 @@ def createLayout():
         [sg.Text('List of the authors articles')],
         [sg.Table(values=[], headings=['Title', 'Authors', 'Publication year', 'citations'],
                   auto_size_columns=False,
-                  col_widths=[20,20,12,12],
+                  col_widths=[20, 20, 12, 12],
                   max_col_width=80,
                   display_row_numbers=True,
                   justification='center',
                   key='-TABLE-',
                   row_height=30)],
-        [sg.Text("Total Citations: 0",key='-CIT-')]
+        [sg.Text("Total Citations: 0", key='-CIT-')]
     ]
-
-    return layout
-
-
-if __name__ == "__main__":
-    sg.theme('DarkAmber')
-
-    layout = createLayout()
 
     window = sg.Window('Part 1', layout, size=(
         1024, 800), element_justification='c')
@@ -87,9 +113,14 @@ if __name__ == "__main__":
             break
 
         if event == 'Submit':
-            (data,citedby) = getAuthorArticles(values[0],values[1])
+            (data,citedby) = getAuthorArticles(values[0], values[1])
             # Fill Table
             window['-TABLE-'].update(data)
             window['-CIT-'].update("Total Citations: " + str(citedby))
 
     window.close()
+
+
+if __name__ == "__main__":
+    sg.theme('DarkAmber')
+    windowModal()
